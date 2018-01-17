ae728bc503ac2b9b9c9fa47d6134891904d037a6
xtreemze <carlos@2activedesign.com>
2018-01-17 15:26:36 +0100

    script update

--- js/master.js
+++ js/master.js
@@ -7,7 +7,10 @@ let ItemsIn = new Set();
 class item {
   constructor(name) {
     this.name = name;
-    this.checkedOut = {};
+    this.returned = {
+      by: "new item",
+      timeStamp: Date.now()
+    };
     ItemsIn.add(this);
   }
 }
@@ -24,77 +27,96 @@ class person {
 let Log = new Set();
 let ItemsOut = new Set();
 class checkOut {
-  constructor(person, item, willReturn) {
-    if (ItemsIn.has(item)) {
-      item.checkedOut = {
-        by: person,
+  constructor(personArg, itemArg, willReturn) {
+    if (ItemsIn.has(itemArg)) {
+      itemArg.checkedOut = {
+        by: personArg.name,
         on: Date.now(),
         nextAvailable: willReturn
       };
-      person.itemsCheckedOut.add(item);
-      ItemsIn.delete(item);
-      ItemsOut.add(item);
+      personArg.itemsCheckedOut.add(itemArg);
+      ItemsIn.delete(itemArg);
+      ItemsOut.add(itemArg);
     }
-    this.by = person.name;
+    if (itemArg.returned) {
+      delete itemArg.returned;
+    }
+    this.by = personArg.name;
     this.timeStamp = Date.now();
     Log.add(this);
   }
 }
 
 class checkIn {
-  constructor(item) {
-    if (ItemsOut.has(item)) {
-      let set = item.checkedOut.by.itemsCheckedOut;
-      set.delete(item);
-      item.checkedOut = { returned: Date.now(), by: item.checkedOut.by };
-      ItemsOut.delete(item);
-      ItemsIn.add(item);
-      this.by = item.checkedOut.by.name;
+  constructor(itemArg) {
+    if (ItemsOut.has(itemArg)) {
+      let personWithItem = itemArg.checkedOut.by;
+      let set = person[personWithItem].itemsCheckedOut;
+      set.delete(itemArg);
+      itemArg.returned = {
+        by: itemArg.checkedOut.by,
+        timeStamp: Date.now()
+      };
+      delete itemArg.checkedOut;
+      ItemsOut.delete(itemArg);
+      ItemsIn.add(itemArg);
+      this.by = itemArg.returned.by;
     }
     this.timeStamp = Date.now();
     Log.add(this);
   }
 }
-function camelize(str) {
-  return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-      return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
-    })
-    .replace(/\s+/g, "");
-}
 
 //Equipment
-let equipmentList = ["GoPro1", "GoPro2", "Phantom3", "Cam360"];
-let items = {};
+let equipmentList = [
+  "GoPro1",
+  "GoPro2",
+  "GoPro3",
+  "Drone",
+  "Cam360",
+  "Car"
+];
 for (let i in equipmentList) {
-  let name = camelize(equipmentList[i]);
-  if (equipmentList.hasOwnProperty(i)) {
-    items[name] = new item(equipmentList[i]);
-  }
+  let name = equipmentList[i];
+  item[name] = new item(equipmentList[i]);
 }
-items;
+
 //People
-let peopleList = ["Carlos Velasco", "Daniel Aberg"];
-let people = {};
+let peopleList = [
+  "Carlos Velasco",
+  "Daniel Åberg",
+  "Malin Melén",
+  "Johanna Stedt",
+  "Mia Kristersson",
+  "Peter Ovgren",
+  "Laura Parsons",
+  "Jimena Castillo",
+  "Elena Bazhenova",
+  "Per-Inge Persson",
+  "Mats Sjöberg",
+  "Svarrer Cornelius",
+  "Helen Thorn Jönsson",
+  "Freddy Kristensson"
+];
+
 for (let i in peopleList) {
-  let name = camelize(peopleList[i]);
-  if (peopleList.hasOwnProperty(i)) {
-    people[name] = new person(peopleList[i]);
-  }
+  let name = peopleList[i];
+  person[name] = new person(peopleList[i]);
 }
-
 // Checkouts
-new checkOut(people.danielAberg, items.cam360, Date.now());
+new checkOut(person["Daniel Åberg"], item["Cam360"], Date.now());
+person;
 
-new checkOut(people.carlosVelasco, items.goPro1, Date.now());
+new checkOut(person["Carlos Velasco"], item["GoPro1"], Date.now());
 
-new checkIn(items.goPro1);
-new checkOut(people.carlosVelasco, items.goPro1, Date.now());
+new checkIn(item["GoPro1"]);
+new checkOut(person["Carlos Velasco"], item["GoPro1"], Date.now());
 
-people;
+new checkIn(item["Cam360"]);
+person;
 People;
 Log;
 ItemsIn;
 ItemsOut;
-let answer = people.carlosVelasco.itemsCheckedOut;
+let answer = person["Carlos Velasco"].itemsCheckedOut;
 answer;
